9b165552c87bfae4f4e81da589c39cfff9136a15
EC2 Default User <ec2-user@ip-172-31-27-69.us-west-2.compute.internal>
2019-11-04 09:04:41 +0000

    adding base layout

---
+++ js/disney.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+           $("#fireHide").hide();
+           
+           $("#fireShow").hover(function() {
+               $('div').show();
+           })
+           
+           
+        })//ready
+        
