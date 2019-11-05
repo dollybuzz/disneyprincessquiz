ad5b1ea301db32c4f056124188b2a24a84168fd5
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-05 06:04:23 +0000

    added javascript functions

--- js/disney.js
+++ js/disney.js
@@ -1,11 +1,58 @@
 $(document).ready(function(){
-    
-           $("#fireHide").hide();
            
+           //global variables
+           var maxScores = [];
+           var largestValue = 0;
+           var yourPrincess = "";
+           var princess = [
+                      {name: "Anna", score:0},
+                      {name: "Cinderella", score:0},
+                      {name: "Cruella", score:0},
+                      {name: "Elsa", score:0},
+                      {name: "Merida", score:0},
+                      {name: "Mulan", score:0},
+                      {name: "Tiana", score:0}
+           ];
+
+    
+           //fireworks header
+           $("#fireHide").hide();
            $("#fireShow").hover(function() {
                $('div').show();
            })
            
+           //functions - must wrap as functions
+           //manipulates scoring
+           for(var i= 0; i < princess.length; i++)
+           {
+                      if(princess[i].name === "Tiana")
+                      {
+                                 princess[i].score += 3;
+                      }
+    
+                      if(princess[i].name === "Mulan")
+                      {
+                                 princess[i].score += 3;
+                      }
+           }
+
+           //parses through the princesses to search for highest score
+           for(i = 0; i < princess.length; i++)
+           {
+                      for( var k = 0; k <princess.length; i++)
+                      {
+                                 maxScores[k] = princess[i].score;
+                                 k++;
+        
+                                 largestValue = Math.max.apply(Math, maxScores);
+
+                                 if(princess[i].score == largestValue)
+                                 {
+                                            yourPrincess = princess[i].name;
+                                 }
+                      }
+                      return console.log(yourPrincess);
+           }
            
-        })//ready
+})//ready
         
